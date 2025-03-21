2fce0e90952391c70e431e656ea3bc33b6e3b0bd
Aengus Roberts <asr66@bath.ac.uk>
2025-03-21 14:52:22 +0000

    Updated FDM for Elliptic ODE for functionality

--- Base implementations/Elliptic_FDM.py
+++ Base implementations/Elliptic_FDM.py
@@ -1,36 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-EPSILON = 0.0000001
+EPSILON = 0.00001
 N = 51
-h = EPSILON * 0.01
 
 D = np.ndarray(shape=(N+1,N+1), dtype=np.float64)
-f = np.ones(shape=N+1, dtype=np.float64)
-f *= h**2
-f[0] = 0
-f[N] = 0
 
-#forming D
-for i in range(0,N+1):
-    if i ==0:
-        D[i,i] = 1
-    elif i == 1:
-        D[i,i] = 2*(EPSILON**2) + h**2
-        D[i,i+1] = -(EPSILON**2)
-    elif i == N-1:
-        D[i,i] = 2*(EPSILON**2) + h**2
-        D[i,i-1] = -(EPSILON**2)
-    elif i == N:
-        D[i,i] = 1
-    else:
-        D[i,i] = 2*(EPSILON**2) + h**2
-        D[i,i-1] = -(EPSILON**2)
-        D[i,i+1] = -(EPSILON**2)
+powers = [i for i in range(-3,3)]
+tens_list = [10**i for i in powers]
+h_list = [EPSILON * ten for ten in tens_list]
 
-u = np.linalg.solve(D, f)
-x = np.linspace(0,1,N+1)
+for k,h in enumerate(h_list):
+    f = np.ones(shape=N+1, dtype=np.float64)
+    f *= h**2
+    f[0] = 0
+    f[N] = 0
 
-#plotting soln
-plt.plot(x,u)
+    #forming D
+    for i in range(0,N+1):
+        if i ==0:
+            D[i,i] = 1
+        elif i == 1:
+            D[i,i] = 2*(EPSILON**2) + h**2
+            D[i,i+1] = -(EPSILON**2)
+        elif i == N-1:
+            D[i,i] = 2*(EPSILON**2) + h**2
+            D[i,i-1] = -(EPSILON**2)
+        elif i == N:
+            D[i,i] = 1
+        else:
+            D[i,i] = 2*(EPSILON**2) + h**2
+            D[i,i-1] = -(EPSILON**2)
+            D[i,i+1] = -(EPSILON**2)
+
+    u = np.linalg.solve(D, f)
+    x = np.linspace(0,1,N+1)
+
+    #plotting soln
+    plt.plot(x,u, label=r'$h = ε\times10^{}$'.format(powers[k]))
+plt.title('FDM: -εu" + u = 1, u(0)=u(1)=0, ε = {}'.format(EPSILON))
+plt.legend()
 plt.show()
